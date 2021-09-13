68514dc855db1e54b288f15140d900cb8ff8bd60
asia <joanna.czerwinska96@gmail.com>
2021-09-13 12:59:52 +0200

    sklep internetowy

---
+++ webShop/src/main/java/com/example/webshop/repository/ProductDao.java
@@ -0,0 +1,31 @@
+package com.example.webshop.repository;
+
+import com.example.webshop.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class ProductDao {
+    private List<Product> products = Arrays.asList(
+            new Product("Mydło", "Pieniące", new BigDecimal("25.00")),
+            new Product("Masło", "Się roztrzasło", new BigDecimal("99.99")),
+            new Product("Chleb", "Razowy", new BigDecimal("5.50")));
+
+    public List<Product> all() {
+        return products;
+    }
+
+
+
+    public Product byName(String name) {
+        for (Product product : products) {
+            if (name.equals(product.getName())) {
+                return product;
+            }
+        }
+        return null;
+    }
+}
